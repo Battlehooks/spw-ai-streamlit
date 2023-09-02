38cdbc7ca1247add440e36dbc2c1fd42b9a0e14a
Rizky Anugrah Ananto <rizkyk592@gmail.com>
2023-09-02 23:38:19 +0700

    Added Token Count

--- db.py
+++ db.py
@@ -26,12 +26,14 @@ class InsertData:
         timestamp = datetime.now()
         question = self.question
         year, month, day, hour, minute, second = timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute, timestamp.second
+        token_pertanyaan = len(self.question.split(' ')) + 2
+        token_jawaban = len(self.jawaban_ai.split(' ')) + 2
         duplicated = self.check_duplicate()
         if duplicated > 0:
             return
-        query = 'INSERT INTO pertanyaan_jawaban (timestamp, pertanyaan, total_jawaban, jawaban_ai) VALUES (%s, %s, %s, %s)'
+        query = 'INSERT INTO pertanyaan_jawaban (timestamp, pert1anyaan, total_jawaban, jawaban_ai, token_pertanyaan, token_jawaban) VALUES (%s, %s, %s, %s, %s, %s)'
         value = (f"{year}-{month}-{day}-{hour}-{minute}-{second}",
-                 question, self.total_answer, self.jawaban_ai)
+                 question, self.total_answer, self.jawaban_ai, token_pertanyaan, token_jawaban)
         self.cursor.execute(query, value)
         self.mydb.commit()
         print(f'{self.cursor.rowcount} record inserted.')
