347dd01a7e7283ddf19ee3ed4e9d0f9449d05ba5
Rizky Anugrah Ananto <rizkyk592@gmail.com>
2023-09-08 15:58:52 +0700

    Temperature Change

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ model_name = 'ft:gpt-3.5-turbo-0613:personal::7sLvXR18'
 embeddings = OpenAIEmbeddings(openai_api_key=api_key)
 db = FAISS.load_local('retriever/FAISS_SPW', embeddings=embeddings)
 model = ChatOpenAI(openai_api_key=api_key, model=model_name,
-                   temperature=0.17)
+                   temperature=0.12)
 result = False
 
 retriever = RetrievalQA.from_chain_type(
